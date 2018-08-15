f1fc792412b34f7bdde9c05d54d626424cade0e0
skfya <42008439+skfya@users.noreply.github.com>
2018-08-15 15:44:28 -0400

    Delete finalproject.js

--- finalproject.js
+++
@@ -1,5 +0,0 @@
-function send() {
-  var response = document.getElementById("reply").value;
-  document.getElementById("response").style.display.block;
-  document.getElementById("anon").innerHTML = response
-}
